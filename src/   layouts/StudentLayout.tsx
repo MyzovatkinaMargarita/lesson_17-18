7439f1051cc56b7891166a1739d568bfbb94d237
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-11-24 01:55:11 +0300

    StudentLayout.tsx

--- src/   layouts/StudentLayout.tsx
+++ src/   layouts/StudentLayout.tsx
@@ -1,1 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+import { NavLink, Outlet } from "react-router-dom";
+import { TestsIcon, ProfileIcon, StatsIcon } from "../icons/icons";
+import Logo from "../assets/Vector.svg";
+
+const Shell = styled.div`
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: 240px 1fr;
+  background: #fff;
+`;
+
+const Aside = styled.aside`
+  border-right: 1px solid #eef1f6;
+  padding: 30px 16px;
+`;
+
+const Main = styled.main`
+  padding: 25px 30px;
+  background-color: #fbfbfb;
+`;
 
