8c65cdb841f7af9d0c242a0460d760065644d6e8
@Margarita52 <muzovatkinamargarita@gmail.com>
2025-11-24 02:31:46 +0300

    router.tsx

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,1 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "../layouts/AppLayout";
+import LoginPage from "../pages/login/LoginPage";
+import StudentPage from "../pages/student/StudentPage";
+import AdminPage from "../pages/admin/AdminPage";
+import NotFoundPage from "../pages/errors/NotFoundPage";
+import StudentTestPage from "./pages/student/StudentTestPage";
+import StudentTests from "./pages/student/StudentTests";
+export const router = createBrowserRouter([
+{
+  path: "student",
+  element: <StudentLayout />,
+  children: [
+    { index: true, element: <Navigate to="tests" replace /> },
+    { path: "tests", element: <StudentTestsPage /> },
+    { path: "statistics", element: <StudentStatsPage /> },
+    { path: "profile", element: <StudentProfilePage /> },
+  ],
+}
+
 
